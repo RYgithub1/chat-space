3dc951955c1aa0f70a206b00c0d4fb0302c7f433
RYgithub1 <ryo.rec.otani@gmail.com>
2020-04-23 15:57:29 +0900

    0422_changedRedundancy

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -25,10 +25,10 @@ pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 #エラーのログを記録するファイルを指定
 # stderr_path "#{app_path}/log/unicorn.stderr.log"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
-stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
 #通常のログを記録するファイルを指定
-stdout_path "#{app_path}/log/unicorn.stdout.log"
+# stdout_path "#{app_path}/log/unicorn.stdout.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
 #Railsアプリケーションの応答を待つ上限時間を設定
 timeout 60
